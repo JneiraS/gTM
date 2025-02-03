e98cc395d126fa58fabc566d1f9cbf76d8b31439
jneira <johnny.neira@gmail.com>
2025-02-03 09:30:31 +0100

    feat(web): améliore la catégorisation des tâches
    * Ajoute la gestion des tâches terminées et en retard
    * Modifie les requêtes SQL pour inclure le statut des tâches
    * Ajoute deux nouvelles variables pour stocker les tâches terminées et en retard
    * Met à jour le template pour afficher toutes les catégories de tâches

--- src/infrastructure/web/views/TaskViews.go
+++ src/infrastructure/web/views/TaskViews.go
@@ -10,6 +10,13 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// Views creates a web server that serves the web interface for the application.
+//
+// The web interface shows four lists of tasks: tasks with a due date, tasks without
+// a due date, tasks that are done, and tasks that are late.
+//
+// The functions to create a new task, mark a task as done and update a task are
+// routed to the corresponding functions in the useCases package.
 func Views() {
 	router := gin.Default()
 	router.LoadHTMLGlob("src/infrastructure/web/templates/*")
@@ -19,24 +26,29 @@ func Views() {
 		db := persistence.CreateDB()
 		var priorityTass []persistence.Task
 		var taskWithoutDueDate []persistence.Task
+		var tasksDone []persistence.Task
+		var lateTasks []persistence.Task
 
-		db.Where("due_date > ? AND due_date < ?", time.Date(1900, 1, 1, 0, 0, 0, 0, time.UTC), time.Now()).
+		db.Where("due_date > ? AND due_date < ? AND status != ?", time.Date(1900, 1, 1, 0, 0, 0, 0, time.UTC), time.Now(), "done").
 			Order("priority ASC, due_date ASC").
 			Find(&priorityTass)
-
-		db.Where("due_date < ?", time.Date(1900, 1, 1, 0, 0, 0, 0, time.UTC)).
+		db.Where("due_date < ? AND status != ?", time.Date(1900, 1, 1, 0, 0, 0, 0, time.UTC), "done").
 			Order("priority ASC").Find(&taskWithoutDueDate)
+		db.Where("status = ?", "done").Find(&tasksDone)
+		db.Where("due_date > ? AND due_date < ? AND status != ?", time.Date(1900, 1, 1, 0, 0, 0, 0, time.UTC), time.Now(), "done").Find(&lateTasks)
 
 		c.HTML(http.StatusOK, "index.tmpl", gin.H{
 			"title":           "Liste des taches",
 			"prioritytasks":   priorityTass,
 			"taskswithoutdue": taskWithoutDueDate,
+			"tasksdone":       tasksDone,
+			"latetasks":       lateTasks,
 		})
 	})
 
 	router.GET("/done/:id", useCases.UpdateTaskHandler(persistence.CreateDB()))
 	router.POST("/", useCases.CreateTaskHandler(persistence.CreateDB()))
 
-	router.Run(":8080")
+	router.Run(":7263")
 
 }
