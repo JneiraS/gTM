493f5e9e0fc91db73440b1152bd3f54c7894d280
jneira <johnny.neira@gmail.com>
2025-02-07 10:10:42 +0100

    feat: améliorer la gestion des titres des tâches
    - Ajouter la possibilité d'écrire et de sauvegarder les titres des tâches directement dans l'interface
    - Permettre à l'utilisateur de modifier le titre d'une tâche en cliquant dessus
    - Envoyer automatiquement la mise à jour du titre à l'API lorsqu'un utilisateur modifie un titre
    - Améliorer l'expérience utilisateur avec un éditeur de texte en place
    - Gérer les erreurs potentielles lors de la mise à jour du titre

--- src/infrastructure/web/static/js/script.js
+++ src/infrastructure/web/static/js/script.js
@@ -27,30 +27,46 @@ document.querySelectorAll(".description").forEach(function (el) {
     });
 });
 
+
 document.querySelectorAll("[class^='task-card-header-'] li").forEach(function (li) {
     li.addEventListener("click", function () {
         this.contentEditable = true;
         this.focus();
 
-        const save = (e) => {
-            if (e.key === "Enter" && (e.ctrlKey || e.metaKey)) {
-                const id = this.closest(".description").id.split("-")[1];
-                fetch("/update-title-task", {
+        const saveTitle = function (event) {
+            if (event.key === "Enter" && (event.ctrlKey || event.metaKey)) {
+                const taskId = this.id.split("-")[1];
+                const taskTitle = this.textContent;
+                fetch(`/update-task-title/`, {
                     method: "POST",
                     headers: {
                         "Content-Type": "application/json"
                     },
                     body: JSON.stringify({
-                        id: id,
-                        description: this.textContent
+                        id: taskId,
+                        title: taskTitle
                     })
-                });
-                this.contentEditable = false;
-                this.removeEventListener("keyup", save);
+                })
+                    .then(function (res) {
+                        if (res.status === 200) {
+                            this.contentEditable = false;
+                            this.removeEventListener("keyup", saveTitle);
+                        } else {
+                            console.log("Error updating task title");
+                        }
+                    })
+                    .catch(function (err) {
+                        console.log("Error updating task title", err);
+                    });
             }
         };
 
-        this.addEventListener("keyup", save);
+        this.addEventListener("blur", () => {
+            this.contentEditable = false;
+            this.removeEventListener("keyup", saveTitle);
+        });
+
+        this.addEventListener("keyup", saveTitle);
     });
 });
 
