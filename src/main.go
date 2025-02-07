bbfd46e253b5b93323cc473e366ad038a70ae5a2
jneira <johnny.neira@gmail.com>
2025-02-07 12:24:01 +0100

    Ssuppression des commentaires

--- src/main.go
+++ src/main.go
@@ -1,27 +1,15 @@
 package main
 
-import "github.com/JneiraS/AMS/src/infrastructure/web/views"
+import (
+	"github.com/JneiraS/AMS/src/infrastructure/web/views"
+)
 
 //"github.com/JneiraS/AMS/src/domain/models"
 //"github.com/JneiraS/AMS/src/infrastructure/persistence"
 //"time"
 
 func main() {
-	views.Views()
-	// db := persistence.CreateDB()
 
-	// persistence.CreateTask(db, persistence.Task{
-	// 	Task: models.Task{
-	// 		Title:         "Test GORM",
-	// 		Description:   "Test Description",
-	// 		DueDate:       time.Now(),
-	// 		Status:        "pending",
-	// 		Priority:      "low",
-	// 		Assignee:      "test-user",
-	// 		Creator:       "admin",
-	// 		Project:       "test-project",
-	// 		Progress:      0,
-	// 		EstimatedTime: 2,
-	// 	},
-	// })
+	views.Views()
+
 }
