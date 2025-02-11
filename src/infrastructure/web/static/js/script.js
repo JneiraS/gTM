d6894f469f768782c8f4d6105d91fd5a5ab2b291
jneira <johnny.neira@gmail.com>
2025-02-11 19:05:48 +0100

    Amélioration de l'affichage

--- src/infrastructure/web/static/js/script.js
+++ src/infrastructure/web/static/js/script.js
@@ -14,7 +14,7 @@ document.querySelectorAll(".description").forEach(function (el) {
                         },
                         body: JSON.stringify({
                             id: id,
-                            description: this.innerHTML.replace(/<div>/g, '\n').replace(/<\/div>/g, '').replace(/<br\s*\/?>/g, '\n').replace(/<pre>/g, '').replace(/<\/pre>/g, '')
+                            description: this.innerHTML.replace(/<div>/g, '\n').replace(/<\/div>/g, '').replace(/<br\s*\/?>/g, '\n').replace(/<pre>/g, '').replace(/<\/pre>/g, '').replace('&gt;', ">")
                         })
                     })
                         .then(response => {
