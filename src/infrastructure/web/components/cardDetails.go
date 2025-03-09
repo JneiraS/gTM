65337bf0a0aa56cf4c81c774f45a71faff2f12d3
jneira <johnny.neira@gmail.com>
2025-03-09 22:48:28 +0100

    documentation & refacto

--- src/infrastructure/web/components/cardDetails.go
+++ src/infrastructure/web/components/cardDetails.go
@@ -4,22 +4,21 @@ import (
 	"fmt"
 
 	"github.com/JneiraS/AMS/src/domain/services"
-	"github.com/JneiraS/AMS/src/infrastructure/persistence"
+	p "github.com/JneiraS/AMS/src/infrastructure/persistence"
 	gom "maragu.dev/gomponents"
 	gomh "maragu.dev/gomponents/html"
 )
 
-func CardDetails(task persistence.Task) gom.Node {
+func CardDetails(task p.Task) gom.Node {
 	return gomh.Section(gomh.Class("detail-task-container"),
 		CardTitle(task.Title),
 		Container("task-details", task),
 	)
 }
 
-func CardComments(task persistence.Task, listOfComment []persistence.Comment) gom.Node {
+func CardComments(task p.Task, listOfComment []p.Comment) gom.Node {
 	return gomh.Section(gomh.Class("detail-task-comment"),
 		gomh.H1(gom.Text("Comments")),
-
 		FormComment(task.ID),
 		ListOfComments(listOfComment),
 	)
@@ -28,44 +27,47 @@ func CardComments(task persistence.Task, listOfComment []persistence.Comment) go
 func CardTitle(title string) gom.Node {
 	return gomh.H1(gom.Text(title))
 }
-func Container(class string, task persistence.Task) gom.Node {
+func Container(class string, task p.Task) gom.Node {
 	return gomh.Div(gomh.Class(class),
-		Paragraph("description_dt", task.Description),
+		ParagraphWithClass("description_dt", task.Description),
 		DetailContainer("task-info", task),
 		ProgressBar(task),
 		TaskTime(task),
 	)
 }
 
-func DetailContainer(class string, task persistence.Task) gom.Node {
+func DetailContainer(class string, task p.Task) gom.Node {
 	return gomh.Div(gomh.Class(class),
 		gom.If(!task.DueDate.IsZero(), KeyValue("Due Date", task.DueDate.Format("2006-01-02"), "fa-solid fa-calendar-xmark")),
 		KeyValue("Status", task.Status, ""),
 		KeyValue("Priority", services.FormatPryority(task.Priority), ""),
-		KeyValue("Assignee", task.Assignee, ""),
-		KeyValue("Creator", task.Creator, ""),
+		KeyValue("Assignee", task.Assignee, "fas fa-user"),
+		KeyValue("Creator", task.Creator, "fas fa-user"),
 		KeyValue("Project", task.Project, ""),
 	)
 }
 
-func Paragraph(class, text string) gom.Node {
+func ParagraphWithClass(class, text string) gom.Node {
 	return gomh.P(gomh.Class(class),
 		gom.Text(text))
 }
 
-func TaskTime(task persistence.Task) gom.Node {
+func TaskTime(task p.Task) gom.Node {
 	return gomh.Div(gomh.Class("task-time"),
 		KeyValue("Estimated Time", fmt.Sprintf("%d hours", task.EstimatedTime), ""),
 		KeyValue("Time Spent", services.FormatTimeSpent(task.TimeSpent), ""),
 	)
 }
 
+// KeyValue returns a p element containing a key-value pair. The icon parameter
+// allows to specify an icon to display before the key-value pair.
 func KeyValue(key, value, icon string) gom.Node {
 	return gomh.P(gomh.I(gomh.Class(icon)), gomh.Strong(gom.Text(key+": ")),
 		gom.Text(value))
 }
 
-func ProgressBar(task persistence.Task) gom.Node {
+// ProgressBar returns a div element representing the progress bar of a task.
+func ProgressBar(task p.Task) gom.Node {
 	return gomh.Div(gomh.Class("task-progress"),
 		gomh.Div(gomh.Class("progress-bar"),
 			gomh.Div(gomh.Class("progress"),
@@ -74,6 +76,7 @@ func ProgressBar(task persistence.Task) gom.Node {
 	)
 }
 
+// FormComment returns a form element for adding a comment to a task with the given id.
 func FormComment(id uint) gom.Node {
 	return gomh.Form(
 		gomh.Class("comment-form"),
@@ -91,29 +94,28 @@ func FormComment(id uint) gom.Node {
 	)
 }
 
-func ListOfComments(listOfComment []persistence.Comment) gom.Node {
+// ListOfComments returns a list element of comments from a slice of comments.
+func ListOfComments(listOfComment []p.Comment) gom.Node {
 	return gomh.Div(gomh.Class("comment-list"),
-		gom.Map(listOfComment, func(comment persistence.Comment) gom.Node {
-			return ListComments(comment)
+		gom.Map(listOfComment, func(comment p.Comment) gom.Node {
+			return Comment(comment)
 		}),
 	)
 }
-func ListComments(listOfComment persistence.Comment) gom.Node {
+
+// Comment returns a comment element for a list of comments.
+func Comment(comment p.Comment) gom.Node {
 	return gomh.Div(
 		gomh.Class("comment"),
-
 		gomh.Div(gomh.Class("comment-time"),
-			gom.Text(listOfComment.CreatedAt.Format("02-01-2006 at 15:04")),
+			gom.Text(comment.CreatedAt.Format("02-01-2006 at 15:04")),
 		),
-
 		gomh.Div(gomh.Class("comment-container-inner"),
-
 			gomh.Div(gomh.Class("comment-author"),
-				gom.Text(listOfComment.Author),
+				gom.Text(comment.Author),
 			),
-
 			gomh.Div(gomh.Class("comment-text"),
-				gom.Text(listOfComment.Text),
+				gom.Text(comment.Text),
 			),
 		),
 	)
