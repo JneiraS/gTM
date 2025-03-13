e5513db4c2cd55f960b3a878c4955caecd65de78
jneira <johnny.neira@gmail.com>
2025-03-13 20:51:27 +0100

    improved

--- src/infrastructure/web/static/js/script-details.js
+++ src/infrastructure/web/static/js/script-details.js
@@ -5,18 +5,17 @@ let listOfSubtask = document.querySelectorAll("[id^='subtask-']");
 listOfSubtask.forEach(subtask => {
     if (subtask.checked) {
         subtask.parentNode.style.textDecoration = "line-through";
+        subtask.parentNode.style.opacity = "0.25";
     }
 
     subtask.addEventListener("click", function () {
         if (this.checked) {
-            this.parentNode.style.textDecoration = "line-through   ";
+            this.parentNode.style.textDecoration = "line-through";
             this.parentNode.style.opacity = "0.25";
         }
         else {
             this.parentNode.style.textDecoration = "none";
             this.parentNode.style.opacity = "1";
-
         }
     });
 });
-
