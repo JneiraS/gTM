5bcc11a046917498a64c4fd5a91be8deac99ff02
jneira <johnny.neira@gmail.com>
2025-03-15 21:19:16 +0100

    refactor(logger): Migrer la journalisation vers un système de fichiers
    * Remplacer les logs stdout par un système d'écriture dans un fichier
    * Ajouter la gestion des erreurs pour les opérations sur fichiers
    * Implémenter les timestamps pour chaque entrée de log
    * Maintenir la compatibilité avec les niveaux de log existants (INFO, WARN, ERROR)

--- src/domain/models/logger.go
+++ src/domain/models/logger.go
@@ -2,19 +2,38 @@ package models
 
 import (
 	"log"
+	"os"
+	"time"
 )
+
+const FILE_NAME_PATH = "log.txt"
 
 type Logger struct {
 }
 
 func (l Logger) LogInfo(message string) {
-	log.Printf("[INFO] %s", message)
+	l.logToFile(FILE_NAME_PATH, "[INFO] "+message)
 }
 
 func (l Logger) LogWarning(message string) {
-	log.Printf("[WARN] %s", message)
+	l.logToFile(FILE_NAME_PATH, "[WARN] "+message)
 }
 
 func (l Logger) LogError(message string) {
-	log.Printf("[ERROR] %s", message)
+	l.logToFile(FILE_NAME_PATH, "[ERROR] "+message)
+}
+
+func (l Logger) logToFile(filename string, content string) {
+	file, err := os.OpenFile(filename, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0644)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer file.Close()
+
+	now := time.Now()
+	_, err = file.WriteString(now.Format("2006-01-02 15:04:05") + " " + content + "\n")
+	if err != nil {
+		log.Println(err)
+	}
 }
